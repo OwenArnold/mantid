962d052f4bb6e40c2a01f3f6c6a188feb7323aec
Owen Arnold <owen.arnold@stfc.ac.uk>
2019-05-07 10:27:51 +0100

    Add mask file and reset
    runs git checkout on Testing directory to reset everything.
    Mask file added to whitelist.

--- white_list.py
+++ white_list.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 import os
+# Reset
+os.system("git checkout Testing/")
 # Files to keep
-whitelist = ['SANS2D00022048.nxs', 'SANS2D00022041.nxs', 'SANS2D00022024.nxs', 'SANS2D00022023.nxs']
+whitelist = ['SANS2D00022048.nxs', 'SANS2D00022041.nxs', 'SANS2D00022024.nxs', 'SANS2D00022023.nxs', 'MaskSANS2DReductionGUI_LimitEventsTime.txt']
 whitelist_search = ['{}.md5'.format(f) for f in whitelist]
 for r, d, f in os.walk('Testing'):
      for file in f:
