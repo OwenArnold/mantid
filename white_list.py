0061e01c3bed2978dd166378039aa99f3e39fec2
Owen Arnold <owen.arnold@stfc.ac.uk>
2019-05-07 15:00:07 +0100

    Demo sans workflow

--- white_list.py
+++ white_list.py
@@ -3,7 +3,7 @@ import os
 # Reset
 os.system("git checkout Testing/")
 # Files to keep. Note an empty white-list is treated as NO filter, will download everything.
-whitelist = ['SANS2D00022048.nxs', 'SANS2D00022041.nxs', 'SANS2D00022024.nxs', 'SANS2D00022023.nxs', 'MaskSANS2DReductionGUI_LimitEventsTime.txt']
+whitelist = ['SANS2D00022048.nxs', 'SANS2D00022041.nxs', 'SANS2D00022024.nxs', 'SANS2D00022023.nxs', 'MaskSANS2DReductionGUI_LimitEventsTime.txt', 'DIRECTM1_15785_12m_31Oct12_v12.dat']
 whitelist_search = ['{}.md5'.format(f) for f in whitelist]
 if whitelist:
     for r, d, f in os.walk('Testing'):
